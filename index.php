035ae436d2b888f42b87c3514e20f9fe03786d03
Deepak <dkk152207@gmail.com>
2017-01-03 12:11:51 +0530

    dfgdr

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ include("header.php");
 include("content.php");
 include("footer.php");
 
-echo "hello deepakdfmvdkfgnkdnfkldfknl";
-echo "All Changed by Pramod Changed";
+//echo "hello deepakdfmvdkfgnkdnfkldfknl";
+//echo "All Changed by Pramod Changed";
 ?>
